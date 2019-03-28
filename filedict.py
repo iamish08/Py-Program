f433f1db4625a4eb1e5abc6fda2fd561d2bd7abe
iamish08 <ashish080397@gmail.com>
2019-03-28 18:03:42 +0530

    files and dictionary

---
+++ filedict.py
@@ -0,0 +1,16 @@
+import string 
+fname=input('Enter the filename:')
+try:
+	fhand=open(fname)
+except:
+	print('File cannot be opened',fname)
+	exit()
+counts=dict()
+for line in fhand:
+	line=line.rstrip()
+	line=line.translate(line.maketrans(',',' ',string.punctuation))
+	line=line.lower()
+	words=line.split()
+	for word in words:
+		counts[word]=counts.get(word,0)+1
+print(counts)
