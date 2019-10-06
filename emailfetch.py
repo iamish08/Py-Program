dae15c32ef3afaa784ee28acfdffb7b686511e97
Aditya <45264407+madhipster517@users.noreply.github.com>
2019-10-06 15:34:23 +0530

    Update emailfetch.py

--- emailfetch.py
+++ emailfetch.py
@@ -3,6 +3,6 @@ fname=input()
 fhand=open(fname)
 for line in fhand:
 	line=line.rstrip()
-	x=re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]',line)
-	if(len(x)>0):
-		print(x)
+	ex=re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]',line)
+	if(len(ex)>0):
+		print(ex)
